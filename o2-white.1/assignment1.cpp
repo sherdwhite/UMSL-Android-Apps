fe0386fd61e4f77a01a22dc4ddc0bb3d7d2710af
Sherd White <sherdwhite@users.noreply.github.com>
2017-09-10 22:22:47 -0500

    Cleaned up Makefile, made changes for 3.8 number 5.

--- o2-white.1/assignment1.cpp
+++ o2-white.1/assignment1.cpp
@@ -3,8 +3,8 @@
 #include <unistd.h>
 int main (int argc, char *argv[]) {
     pid_t childpid = 0;
-    int i, n;
-    if (argc != 2){ /* check for valid number of command-line arguments */
+    int i, n, k, m;
+    if (argc != 4){ /* check for valid number of command-line arguments */
         fprintf(stderr, "Usage: %s processes\n", argv[0]);
     return 1;
     }
@@ -13,7 +13,8 @@ int main (int argc, char *argv[]) {
     for (i = 1; i < n; i++)
         if (childpid = fork())
         break;
-	sleep(10);
-    fprintf(stderr, "i:%d process ID:%ld parent ID:%ld child ID:%ld\n", i, (long)getpid(), (long)getppid(), (long)childpid);
-    return 0;
+	for (i = 1; i < k; i++)
+		fprintf(stderr, "i:%d process ID:%ld parent ID:%ld child ID:%ld\n", i, (long)getpid(), (long)getppid(), (long)childpid);
+		sleep(m);
+	return 0;
 }
