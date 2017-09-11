51047496fc6db4e305d2c225478ec224627fc5f4
Sherd White <sherdwhite@users.noreply.github.com>
2017-09-10 22:35:33 -0500

    Reverted to 3.1, added wait before final fprintf per 6.

--- o2-white.1/assignment1.cpp
+++ o2-white.1/assignment1.cpp
@@ -3,22 +3,18 @@
 #include <unistd.h>
 int main (int argc, char *argv[]) {
     pid_t childpid = 0;
-    int i, n, k, m;
-    if (argc != 4){ /* check for valid number of command-line arguments */
+    int i, n;
+    if (argc != 2){ /* check for valid number of command-line arguments */
         fprintf(stderr, "Usage: %s processes\n", argv[0]);
     return 1;
     }
 
     n = atoi(argv[1]);
-	k = atoi(argv[2]);
-	m = atoi(argv[3]);
-	
     for (i = 1; i < n; i++)
         if (childpid = fork())
         break;
-	for (i = 1; i < k; i++) {
-		fprintf(stderr, "i:%d process ID:%ld parent ID:%ld child ID:%ld\n", i, (long)getpid(), (long)getppid(), (long)childpid);
-		sleep(m);
-	}
-	return 0;
+	
+	wait;
+    fprintf(stderr, "i:%d process ID:%ld parent ID:%ld child ID:%ld\n", i, (long)getpid(), (long)getppid(), (long)childpid);
+    return 0;
 }
