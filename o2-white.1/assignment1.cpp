5daff05442430b88619aa9473f2409229d5ef554
Sherd White <sherdwhite@users.noreply.github.com>
2017-09-14 18:01:35 -0500

    Attempting getopt

--- o2-white.1/assignment1.cpp
+++ o2-white.1/assignment1.cpp
@@ -15,11 +15,34 @@ int main (int argc, char *argv[]) {
 		perror("Wrong number of arguements given!");
     return 1;
     }
+	
+	while ((c = getopt (argc, argv, "n:c:")) != -1)
+    switch (c)
+		  {
+			case 'n':
+				nvalue = optarg;
+				break;
+			case 'c':
+				cvalue = optarg;
+				break;
+			case '?':
+				if (optopt == 'n')
+					fprintf (stderr, "Option -%c requires an argument.\n", optopt);
+				if (optopt == 'c')
+					fprintf (stderr, "Option -%c requires an argument.\n", optopt);
+				else if (isprint (optopt))
+					fprintf (stderr, "Unknown option `-%c'.\n", optopt);
+				else
+					fprintf (stderr, "Unknown option character `\\x%x'.\n", optopt);
+					return 1;
+			  default:
+				abort ();
+		  }
 
-    n = atoi(argv[1]);
+    n = atoi(nvalue);
 	// k = atoi(argv[2]);  // 3.8 #5
 	// m = atoi(argv[3]);  // 3.8 #5
-	nchars = atoi(argv[2]);
+	nchars = atoi(cvalue);
 	char mybuf[nchars];
 
     std::cout <<  "Please enter " << nchars << " characters:";
