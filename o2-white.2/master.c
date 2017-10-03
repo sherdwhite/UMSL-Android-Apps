6b3de91571091312e341e20499b68bd83b49764d
Sherd White <sherdwhite@users.noreply.github.com>
2017-10-03 00:17:33 -0500

    Shared memory working.

--- o2-white.2/master.c
+++ o2-white.2/master.c
@@ -27,14 +27,14 @@ int main(int argc, char * argv[])
 		// list.push_back(loc);
 	// }
 	typedef struct {
-		int shm_id, index, flag;
-		//key_t key;
-		char data[SIZE];
+		int flag;
+		int index;  //key_t key;
+		char *data[SIZE];
 	} shared_memory;
 
 	// create shared memory segment and get the segment id
-	// IPC_PRIVATE, child process, created after the parent has obtained the 
-	// shared memory, so that the private key value can be passed to the child 
+	// IPC_PRIVATE, child process, created after the parent has obtained the
+	// shared memory, so that the private key value can be passed to the child
 	// when it is created.  Key could be arbitrary integer also.
 	// IPC_CREAT says to create, but don't fail if it is already there
 	// IPC_CREAT | IPC_EXCL says to create and fail if it already exists
@@ -42,9 +42,9 @@ int main(int argc, char * argv[])
 	int shm_id = shmget(IPC_PRIVATE, sizeof(shared_memory), PERM | IPC_CREAT | IPC_EXCL);
     if (shm_id == -1) {
         perror("Failed to create shared memory segment");
-		return 1;
+                return 1;
 	}
-	
+
 	printf("My segment id is %d\n", shm_id);
 
 	// attach shared memory segment
@@ -52,26 +52,32 @@ int main(int argc, char * argv[])
 	// shmat(segment_id, NULL, SHM_RDONLY) to attach to read only memory
     if (ptr == (void*)-1) {
         perror("Failed to attach shared memory segment");
-		return 1;
-	}
-	
+                return 1;
+        }
+
 	printf("My ptr address is %x\n", ptr);
-	
-	ptr->index = 0;
-	ptr->flag = 0;
-	ptr->data = 0;
-	
+
+	ptr->flag  = 1;
+	ptr->data[0] = "shit";
+	ptr->index  = 2;
 	// We can use the shared_memory ptr to get access to shared memory.
 	// Could also be ptr[0].index or ptr[0].flag, etc.
-	printf("The index is %n, the flag is %n, the array is %s.", ptr->index, ptr->flag, ptr->data);  
+	printf("The index is %d, the flag is %d, the array is %s.\n", ptr->index, ptr->flag, ptr->data[0]);
 
 	// could add to data via scanf("%
-	
+
 	// for(i = 0; i < 5; i++) {
 	// critical seciton
 	// Code for each child in palin.cpp
-			//wait(1);
+					//wait(1);
 	// }
+    /*
+     * Wait until the other process
+     * changes the first character of our memory
+     * to '*', indicating that it has read what
+     * we put there.
+     */
+
     /*
      * Wait until the other process
      * changes the first character of our memory
