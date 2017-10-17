46f8ed58b48a98216d54b5db2828e2cf8cf73a37
Sherd White <sherdwhite@users.noreply.github.com>
2017-10-17 16:53:52 -0500

    Test

--- o2-white.3/user.c
+++ o2-white.3/user.c
@@ -111,7 +111,7 @@ int main(int argc, char * argv[])
 	printf("Child: %d, Semaphore value is %d. \n", pid, sem_value);
 	int clear = 0;
 	while(clear = 0){
-		sem_wait(sem);  // wait until we can subtract 1
+		while(sem_wait(sem));  // wait until we can subtract 1
 		printf("Child: %d sem_wait. \n", pid);
 		// Critical Section
 		if(nano_end < shared->nanoseconds && sec_end < shared->seconds){
